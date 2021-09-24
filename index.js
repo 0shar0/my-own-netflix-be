80f2fc7aed02e61a2ca00123064cce0bce7be0b9
Shar4ik <bodiasharko@gmail.com>
2021-09-24 23:52:34 +0300

    edit my app

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
-const client = require('./pg');
-const port = process.env.PORT;
 
 const app = express();
 
-/*
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    /!*await client.connect();*!/
-
+    app.listen(process.env.PORT, () => console.log('server start'));
   } catch (e) {
     console.log(e);
   }
 };
 
-/!*start();*!/
-*/
-
-app.get('/', (r,re) => {
-  re.end(`<div>response</div>`);
-});
-
-app.listen(port, () => console.log('server start'));
+start();
