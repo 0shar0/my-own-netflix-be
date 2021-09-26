3c479e94351137c09e7473593e9c0a9630ea66f6
Shar4ik <bodiasharko@gmail.com>
2021-09-26 19:56:46 +0300

    edit my app

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../models/models');
 
 const getJWT = (id, email, role) => {
-  return jwt.sign({ id, email, role }, 'some_secret_key', {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
     expiresIn: '24h',
   });
 };
