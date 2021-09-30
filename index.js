24b47c965d9152132ccc341a743266e18cb212dd
Shar4ik <bodiasharko@gmail.com>
2021-10-01 00:35:17 +0300

    edit my app

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middlewear/ErrorHandlingMidldewear');
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 app.use(errorHandler);
